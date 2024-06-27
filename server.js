7a6d6d805895c84998e97aadd6ff09f8ae8ee0c5
fasill <fasilhawultie19@gmail.com>
2024-06-27 10:10:17 +0300

    [Folder Structure Created] I have created the folder structure for the project i

---
+++ server.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import express from "express";
+import router from "./router/index"
+import cors from "cors";
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use(cors());
+
+app.use('/',router.auth);
+app.use('/',router.chat);
+
+var port = 8080;
+mongoose.connect(
+    process.env.db_connection_link)
+    .then(() => app.listen(port))
+    .then(() => console.log(`Server started on port http://localhost:${port}/`))
+    .catch((err)=>console.log(err));
